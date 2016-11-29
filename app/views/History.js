9cbc6d49a4b1e715c01a9c65a1f65c559026ada3
MacKentoch <erwan.datin@gmail.com>
2016-11-29 21:16:57 +0100

    History

--- app/views/History.js
+++ app/views/History.js
@@ -1,8 +1,11 @@
-import React, {
-  Component
-}                     from 'react';
-import cx             from 'classnames';
+/* eslint react/jsx-closing-bracket-location:0 */
+/* eslint react/sort-comp:0 */
+/* eslint arrow-body-style:0 */
+
+import React, { Component } from 'react';
 import shallowCompare from 'react-addons-shallow-compare';
+import ViewContainer from '../components/ViewContainer';
+import ViewTitle from '../components/ViewTitle';
 
 class History extends Component {
   state = {
@@ -10,29 +13,28 @@ class History extends Component {
     viewEntersAnim: true
   };
 
-
   shouldComponentUpdate(nextProps, nextState) {
     return shallowCompare(this, nextProps, nextState);
   }
 
   render() {
     const { animated, viewEntersAnim } = this.state;
-    return(
-      <div
-        className={
-            cx({
-            'animatedViews': animated,
-            'view-enter': viewEntersAnim
-          })
-        }>
+    return (
+      <ViewContainer
+        animated={animated}
+        launchAnimation={viewEntersAnim}>
         <div className="row">
           <div className="col-sm-12">
-            <h1>
-              History
-            </h1>
+            <ViewTitle
+              title={'History'}
+              faIconName={'fa-upload'}
+            />
+            <div>
+              content to add
+            </div>
           </div>
         </div>
-      </div>
+      </ViewContainer>
     );
   }
 }
