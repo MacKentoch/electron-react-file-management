0af27e07c664b74f51752471c5219d8c03024b41
MacKentoch <erwan.datin@gmail.com>
2016-12-10 08:01:04 +0100

    add config menu

--- app/models/sidemenu.js
+++ app/models/sidemenu.js
@@ -1,4 +1,7 @@
+/* eslint import/prefer-default-export:0 */
+
 export const sidemenuModel = [
-  {id: 1, text: 'Upload', linkTo: '/', fontIconName: 'fa-upload'},
-  {id: 2, text: 'History', linkTo: '/history', fontIconName: 'fa-history'}
+  { id: 1, text: 'Upload', linkTo: '/', fontIconName: 'fa-upload' },
+  { id: 2, text: 'History', linkTo: '/history', fontIconName: 'fa-history' },
+  { id: 3, text: 'Config.', linkTo: '/info', fontIconName: 'fa-info-circle' }
 ];
